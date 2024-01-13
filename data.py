3f06f73498b6c513129d9694de0b9f51cbad30fa
newproject23 <143152902+newproject23@users.noreply.github.com>
2024-01-13 06:32:24 +0300

    Update data.py

--- data.py
+++ data.py
@@ -2,27 +2,27 @@ from pyrogram.types import InlineKeyboardButton
 
 
 class Data:
-    generate_single_button = [InlineKeyboardButton("↯︙ انشاء كود بايروجرام .", callback_data="generate")]
+    generate_single_button = [InlineKeyboardButton("↯︙ انشاء جلسه .", callback_data="generate")]
 
     generate_button = [generate_single_button]
 
     buttons = [
         generate_single_button,
-        [InlineKeyboardButton("↯︙المطور .", url="https://t.me/j_s_9"),
+        [InlineKeyboardButton("↯︙𝑀𝐴𝑅𝑂 .", url="https://t.me/j_s_9"),
         ],
 
-        [InlineKeyboardButton("↯︙المطور .", url="https://t.me/vip_alpop"),
+        [InlineKeyboardButton("↯︙𝐴𝐿𝑃𝑂𝑃 .", url="https://t.me/vip_alpop"),
         ],
 
-        [InlineKeyboardButton("↯︙السورس .", url="https://t.me/source_alpop"),
+        [InlineKeyboardButton("↯︙𝑆𝑂𝐔𝑅𝐶𝐸 𝐴𝐿𝑃𝑂𝑃 .", url="https://t.me/source_alpop"),
         ],
 
-        [InlineKeyboardButton("↯︙جروب الدعم .", url="https://t.me/bar_alpop"),
+        [InlineKeyboardButton("↯︙𝐺𝑅𝑂𝐔𝑃 .", url="https://t.me/bar_alpop"),
         ],
     ]
 
     START = """
-↯︙اهلا بك في بوت انشاء كود بايروجرام .
+↯︙اهلا بك في بوت انشاء الجلسات .
 
 ↯︙ يمكنك انشاء كود بايروجرام بسهولة .
 
